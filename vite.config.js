5db1e5d162ffe28c91658351620308163f187902
LINTAREAS <ts202534004@alm.buap.mx>
2026-04-21 15:16:40 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/prueba-de-riders/', // 👈 nombre exacto de tu repo
+  base: '/prueba-de-riders/',
 })
