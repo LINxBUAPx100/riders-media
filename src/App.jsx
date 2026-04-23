a01bb1960293184a8644008491d613411f4f3765
LINTAREAS <ts202534004@alm.buap.mx>
2026-04-23 17:50:13 -0600

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ function HomeView({ nav }) {
           ) : (
             <iframe 
               style={{ position: "absolute", inset: 0, width: "100%", height: "100%", border: "none" }}
-              src="https://player.vimeo.com/video/TU_ID_DE_VIMEO?autoplay=1&title=0&byline=0&portrait=0" 
+              src="https://www.youtube.com/embed/hB7CDrVnNCs" 
               allow="autoplay; fullscreen; picture-in-picture" 
               allowFullScreen
               title="Showreel Riders Media"
